bfd00f98b9699b35ceac9f8711e98c1ba0ee3074
Miguel Gemio <miguel.gemio@jala.university>
2022-10-04 13:39:38 -0400

    Files to read files and Output files added

---
+++ src/output_helper.py
@@ -0,0 +1,22 @@
+import csv
+import uuid
+from datetime import datetime
+
+OUTPUT_PATH = '/Users/miguelgemio/Jala/Python-Backend/Assignements/final_assig/movie_explorer/output/'
+REQUEST_FILENAME = 'request.csv'
+
+
+def write_on_csv_request(request_data):
+    answer = {'statusCode': 'error', 'body': None}
+    if request_data:    
+        path = OUTPUT_PATH + REQUEST_FILENAME
+        unique_id, date = uuid.uuid4().hex, datetime.now().replace(microsecond=0)
+        new_row_data = [unique_id, request_data, date]
+        try:
+            with open(path, 'a') as csv_file:
+                writer = csv.writer(csv_file)
+                writer.writerow(new_row_data)
+            answer = {'statusCode': 'ok', 'body': None}
+        except Exception as e:
+            print('Something bad happened', e)
+    return answer
---
+++ test/test_output_helper.py
@@ -0,0 +1,36 @@
+from src.output_helper import write_on_csv_request
+
+OUTPUT_PATH = '/Users/miguelgemio/Jala/Python-Backend/Assignements/final_assig/movie_explorer/output/'
+REQUEST_FILENAME = 'request.csv'
+
+def test_write_csv_request_output_returns_ok():
+    # Arrange
+    filename = REQUEST_FILENAME
+    data = f'Test {filename} - ok'
+    expected_output = {'statusCode': 'ok', 'body': None}
+
+    # Act
+    output = write_on_csv_request(data)
+
+    # Assert
+    assert output is not None, 'expected non null response'
+    assert type(output) is dict, 'expected response as `dict`'
+    assert output == expected_output, 'output did not match expected output'
+    assert output.get('statusCode') == 'ok', 'expected `ok` statusCode'
+    assert output.get('body') is None, 'expected `None` body'
+
+
+def test_write_csv_request_not_write_empty_data_returns_error():
+     # Arrange
+    data = ''
+    expected_output = {'statusCode': 'error', 'body': None}
+
+    # Act
+    output = write_on_csv_request(data)
+
+    # Assert
+    assert output is not None, 'expected non null response'
+    assert type(output) is dict, 'expected response as `dict`'
+    assert output == expected_output, 'output did not match expected output'
+    assert output.get('statusCode') == 'error', 'expected `error` statusCode'
+    assert output.get('body') is None, 'expected `None` body'
