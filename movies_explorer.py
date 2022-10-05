e79a6653528335f0eb37b9ba32d8b5f61d8c79fc
Jose Miguel Gemio Quispe <67058873+miguelitogemio96@users.noreply.github.com>
2022-10-05 18:45:16 -0400

    Write results in json files (#3)
    Co-authored-by: Miguel Gemio <miguel.gemio@jala.university>

--- movies_explorer.py
+++ movies_explorer.py
@@ -1,7 +1,12 @@
-from src.parser_helper import create_new_parser
+from textwrap import indent
+import uuid
+import json
+from src.parser_helper import create_new_parser, is_feature_2, is_feature_3, is_feature_4, \
+    is_feature_5, is_feature_6, is_feature_7, is_feature_8
+from src.output_helper import save_output_in_json_csv_file
 
 # Read the Argument Since the console
-req = create_new_parser()    # We get the arguments.
+req, str_args = create_new_parser()    # We get the arguments in a dict and str.
 # It returns a Dictionary with the formar:
 # {
 #     'title' : args.title ,
@@ -14,18 +19,71 @@ req = create_new_parser()    # We get the arguments.
 #     'user' : args.user,
 # }
 if req:
-    if req.get('title') and len(req.get('title')) > 1 and \
-        req.get('order') and req.get('order_by'):
-        # For feature 2 do here
+    output = []
+    if is_feature_2(req):
+        # For feature 2 do here[{sa},{wqer}].
         print('feature 2')
-        pass
-    elif req.get('title') and len(req.get('title')) == 1 and \
-        req.get('order') and req.get('order_by'):
-        # For feature 3 do here
+        # output = feature2(req) 
+        
+    elif is_feature_3(req):
+        # For feature 3 do here.
         print('feature 3')
+        # output = feature2(req) output wait for a list of dicts [{..},{..}].
+        
+    elif is_feature_4(req):
+        # For feature 3 do here.
+        print('feature 4')
+        # output = feature2(req) output wait for a list of dicts [{..},{..}].
+
+    elif is_feature_5(req):
+        # For feature 3 do here.
+        print('feature 5')
+        # output = feature5(req) output wait for a list of dicts [{..},{..}].
+
+    elif is_feature_6(req):
+        # For feature 3 do here.
+        print('feature 6')
+        # output = feature6(req) output wait for a list of dicts [{..},{..}].
+
+    elif is_feature_7(req):
+        # For feature 3 do here.
+        print('feature 7')
+        # output = feature7(req) output wait for a list of dicts [{..},{..}].
+
+    elif is_feature_8(req):
+        # For feature 3 do here.
+        print('feature 8')
+        # output = feature8(req) output wait for a list of dicts [{..},{..}].
+
     else:
-        print('Te arguments are not te corrects',
+        print('The arguments are not te corrects',
               'to more information type "movies_explorer.py --help"')
+    
+    # example of output
+    # output = [
+	# 	{
+	# 		"title": "ABCDEFG",
+	# 		"release_date": 1934,
+	# 		"genres": ["acd", "xyz", "fgh"]
+	# 	},
+	# 	{
+	# 		"title": "OPQRST",
+	# 		"release_date": 1950,
+	# 		"genres": ["acd"]
+	# 	}
+    # ]
+    # Here we are gonna print te ourput as a json and we will add the request to csv
+    # and create a new json object with the output content and assigning a uuid.
+    unique_id = uuid.uuid4().hex
+    dict_output = {
+        'request_id': unique_id,
+        'data': output,
+        'size': len(output)
+    }
+    json_output = json.dumps(dict_output, indent=4)
+    save_output_in_json_csv_file(str_args, dict_output, unique_id)
+    print(json_output)
+    
 else:
     print('Yo did not type any value for the arguments',
           'to more information type "movies_explorer.py --help"')
--- src/output_helper.py
+++ src/output_helper.py
@@ -1,5 +1,6 @@
 import csv
 import uuid
+import json
 from datetime import datetime
 
 OUTPUT_PATH = './output/'
@@ -19,4 +20,27 @@ def write_on_csv_request(request_data):
             answer = {'statusCode': 'ok', 'body': None}
         except Exception as e:
             print('Something bad happened', e)
+    return answer
+
+
+def save_output_in_json_csv_file(str_request, output_data, unique_id):
+    answer = {'statusCode': 'error', 'body': None}
+    path_csv = OUTPUT_PATH + REQUEST_FILENAME
+    path_json = OUTPUT_PATH + unique_id + '.json'
+    try:
+        current_date = datetime.now().replace(microsecond=0)
+        new_row_data = [unique_id, str_request, current_date]
+        
+        json_data = json.dumps(output_data, indent=4)
+        with open(path_csv, 'a') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow(new_row_data)
+        
+        with open(path_json, 'w+') as json_file:
+            json_file.write(json_data)
+            
+        answer = {'statusCode': 'ok', 'body': None}
+    except Exception as e:
+        print('Something bad happened', e)
+    
     return answer
