08acce48dd3c0585ee80c6f6567ebc7534678cbb
Jose Miguel Gemio Quispe <67058873+miguelitogemio96@users.noreply.github.com>
2022-10-05 23:25:51 -0400

    Andre Complete the Feature 2 (#4)
    * get directory
    
    * merge main
    
    Co-authored-by: John Doe <hinojosa1055@gmail.com>

---
+++ src/filter_handler.py
@@ -0,0 +1,28 @@
+from components.filters_string import *
+from file_helper import read_data
+
+def get_dictionary() -> dict:
+    
+    data = []
+    csvReader= read_data('movies')
+
+    for rows in csvReader:
+        data.append(set_data_title_release_date_genres(rows))
+    
+    return data
+
+def set_data_title_release_date_genres(rows) ->dict:
+    return {'title':normalized(delate_string(helper_find(rows['title']), rows['title'])[:-1]),
+    'release_date':filter_handler(helper_find(rows['title'])),
+    'genres':genres_handler(rows['genres'])}
+    
+def filter_by_all_title_decen_acend(acendent) -> dict:
+    data_json = get_dictionary()
+    sorted_dict = sorted(data_json, key=lambda x: x["title"],reverse=acendent)
+    return sorted_dict
+
+def filter_list_all_movies_with_rating_of_3():
+    return 0
+
+if __name__ == '__main__':
+    print(filter_by_all_title_decen_acend(False))
